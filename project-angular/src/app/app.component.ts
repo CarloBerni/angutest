d2de9e596fc3589aca2c565154db05b6b0560878
CarloBerni <carlo.berni@hetic.net>
2019-07-01 15:41:08 +0200

    add custom directive

--- project-angular/src/app/app.component.ts
+++ project-angular/src/app/app.component.ts
@@ -12,4 +12,14 @@ export class AppComponent {
   "June", "July", "August", "September",
   "October", "November", "December"];
   isavailable = true;
+
+  myClickFunction(event) { 
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Button is clicked");
+    console.log(event);
+ }
+ changemonths(event) {
+  alert("Changed month from the Dropdown");
+  console.log(event);
+}
 }
